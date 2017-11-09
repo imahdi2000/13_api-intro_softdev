90a0e7d0cde7dfb22e0cb8dc2869eb3319a9e837
imahdi2000 <imahdi@stuy.edu>
2017-11-09 18:36:09 -0500

    blast off

---
+++ nasa.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import urllib2, json
+
+my_app = Flask(__name__)
+
+@my_app.route('/')
+def root():
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=t36Ut0AZaGzL4mvtyCVaIKciVYrvDLoleqChpBkW")
+    data_string = u.read()
+    dic = json.loads(data_string)
+    picture = dic["url"]
+    comment = dic["explanation"]
+    print "done"
+    print dic["url"]
+    return render_template('nasa.html' , picture = picture , comment = comment)
+
+if __name__ == '__main__':
+   my_app.debug = True
+   my_app.run() #run the web app
