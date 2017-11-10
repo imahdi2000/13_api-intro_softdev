6a6a43db9f1effc2177243b4e36885dd3e0c2079
imahdi2000 <imahdi@stuy.edu>
2017-11-10 14:45:21 -0500

    new stuffs

--- nasa.py
+++ nasa.py
@@ -3,8 +3,14 @@ import urllib2, json
 
 my_app = Flask(__name__)
 
+
+
 @my_app.route('/')
 def root():
+    return render_template('choose.html')
+
+@my_app.route('/nasa')
+def nasa():
     u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=t36Ut0AZaGzL4mvtyCVaIKciVYrvDLoleqChpBkW")
     data_string = u.read()
     dic = json.loads(data_string)
